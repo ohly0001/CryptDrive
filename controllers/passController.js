0ffc81bba7c5d9f9a9b9ad98b3db17a4fb075969
Robert Ohly <ohly0001@algonquinlive.com>
2026-02-06 19:38:49 -0500

    added temp pwd cpy

--- controllers/passController.js
+++ controllers/passController.js
@@ -1,4 +1,6 @@
 import Password from '../models/password.js';
+import Account from '../models/account.js';
+import { decrypt } from '../utilities/aes.js';
 
 const pull = async (req, res, next) => {
     try {
@@ -29,6 +31,29 @@ const pull = async (req, res, next) => {
     }
 };
 
+const copyUsername = async (req, res, next) => {
+    try {
+        if (!req.isAuthenticated?.() || !req.user) {
+            return res.status(401).send('Not authenticated.');
+        }
+
+        const { passwordId } = req.body;
+
+        const pwd = await Password.findById(passwordId);
+        if (!pwd || !pwd.account.equals(req.user._id)) {
+            return res.status(404).send('Password not found.');
+        }
+
+        const secretKey = req.user.decodeSecretKey();
+        const decryptedUsername = decrypt(pwd.username, secretKey);
+
+        res.json({ username: decryptedUsername });
+    } catch (err) {
+        next(err);
+    }
+};
+
 export default {
-    pull
+    pull, 
+    copyUsername
 };
